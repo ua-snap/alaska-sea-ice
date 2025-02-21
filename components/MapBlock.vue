daae2b8c1ded63d8f65eb85ece6cf00ef97f0a65
Bob Torgerson <rltorgerson@alaska.edu>
2025-02-21 09:23:19 -0900

    Pulling more of the structure over from other applications to more closely mirror their file system structure.

---
+++ components/MapBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="columns">
+    <div class="column is-one-third mt-3 ml-4">
+      <slot name="layers"></slot>
+    </div>
+    <div class="column">
+      <Map :mapId="mapId" :crs="crs" class="map" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  mapId: string;
+  crs?: string;
+}>();
+</script>
+
+<style lang="scss" scoped></style>
