36c47a5e0dcd012c33916fe06ad838d6f31b5604
Bob Torgerson <rltorgerson@alaska.edu>
2025-02-21 09:44:36 -0900

    Adds map layers and the ability to use circumpolar map like in ARDAC.

--- stores/map.ts
+++ stores/map.ts
@@ -16,6 +16,23 @@ export const useMapStore = defineStore("map", () => {
   // Leaflet map objects, keys equal to ID of Leaflet map
   const maps: { [index: string]: any } = {};
 
+  // Active layer Leaflet objects, keyed like `maps` var above
+  var layerObjects: { [index: string]: any } = {};
+
+  // Leaflet control (legend) objects, keyed like `maps` var above
+  var legendControls: { [index: string]: any } = {};
+
+  // Legend items for each map, keyed like `maps` var above.
+  var legendItems: { [index: string]: any } = {};
+
+  var coastlineLayer: TileLayer.WMS;
+  var circumpolarPlaces: L.GeoJSON;
+  var mask: TileLayer.WMS;
+  var baseLayer: TileLayer | null;
+  var southWest: LatLng;
+  var northEast: LatLng;
+  var viscosity = 0.0;
+
   // Active layers for each map
   const activeLayers: Ref<Record<string, MapLayer>> = ref({});
 
@@ -24,10 +41,6 @@ export const useMapStore = defineStore("map", () => {
     let resolutions: number[];
     let center: L.LatLng;
     let zoom: number;
-    let baseLayer: TileLayer | null;
-    let southWest: L.LatLng;
-    let northEast: L.LatLng;
-    let viscosity = 0.0;
 
     if (crs === "EPSG:3572") {
       resolutions = [12000, 6000, 3000, 1500];
@@ -106,9 +119,226 @@ export const useMapStore = defineStore("map", () => {
     }
   }
 
+  // `legends` is an object with keys corresponding to the name of the
+  // legend to use for the layer, and values which is an array of legend items.
+  function setLegendItems(
+    mapId: string,
+    legends: Record<string, LegendItem[]>,
+  ) {
+    legendItems[mapId] = legends;
+  }
+
+  // legendKey = the `legend` property in the definition of a MapLayer.
+  function addLegend(mapId: string, legendKey: string) {
+    // Legends must be provided by the component rendering
+    // the map via the `setLegends` method.
+    if (!legendItems[mapId]) {
+      throw `Legend items not set for map ID '${mapId}' in mapStore`;
+    }
+
+    // Remove the active map legend, if present
+    if (legendControls[mapId]) {
+      legendControls[mapId].remove();
+    }
+
+    legendControls[mapId] = new $L.Control({ position: "topleft" });
+    legendControls[mapId].onAdd = (map: L.Map) => {
+      var div = $L.DomUtil.create("div", "info legend");
+      div.innerHTML = "";
+      legendItems[mapId][legendKey].forEach((legendItem: LegendItem) => {
+        div.innerHTML +=
+          '<div class="legend-item"><div class="legend-swatch" style="background-color: ' +
+          legendItem["color"] +
+          ';"></div> ' +
+          legendItem["label"] +
+          "</div>";
+      });
+      return div;
+    };
+    legendControls[mapId].addTo(maps[mapId]);
+  }
+
+  function toggleLayer(layerObj: MapLayerInstance) {
+    // Remove existing active layer, coastline, and mask from map
+    if (layerObjects[layerObj.mapId]) {
+      maps[layerObj.mapId].removeLayer(layerObjects[layerObj.mapId]);
+      if (
+        circumpolarPlaces &&
+        maps[layerObj.mapId]?.hasLayer(circumpolarPlaces)
+      ) {
+        maps[layerObj.mapId].removeLayer(circumpolarPlaces);
+      }
+      if (mask && maps[layerObj.mapId]?.hasLayer(mask)) {
+        maps[layerObj.mapId].removeLayer(mask);
+      }
+      if (
+        coastlineLayer != undefined &&
+        maps[layerObj.mapId].hasLayer(coastlineLayer)
+      ) {
+        maps[layerObj.mapId].removeLayer(coastlineLayer);
+      }
+    }
+
+    let layer = layerObj.layer;
+    let crs = maps[layerObj.mapId].options.crs.code;
+
+    // Add EPSG:3572 mask layer first to hide map artifacts before they load
+    if (crs == "EPSG:3572") {
+      mask = tileLayer.wms(config.public.geoserverUrl, {
+        transparent: true,
+        format: "image/png",
+        layers: "ardac:cmip6_mask",
+        styles: "ardac:cmip6_mask",
+        zIndex: 20,
+      });
+      maps[layerObj.mapId]?.addLayer(mask);
+    }
+
+    let layerConfiguration = {
+      transparent: true,
+      format: "image/png",
+      version: "1.3.0",
+      layers: layer.wmsLayerName,
+      id: layer.id,
+      styles: layer.style,
+      ...layer.rasdamanConfiguration,
+    };
+
+    let wmsUrl =
+      layer.source == "rasdaman"
+        ? config.public.rasdamanUrl
+        : config.public.geoserverUrl;
+
+    layerObjects[layerObj.mapId] = tileLayer.wms(wmsUrl, layerConfiguration);
+    maps[layerObj.mapId]?.addLayer(layerObjects[layerObj.mapId]);
+
+    if (crs == "EPSG:3572") {
+      addCircumpolarPlaces(layerObj.mapId);
+    }
+
+    if (layerObj.layer.bbox) {
+      const bounds = latLngBounds(
+        latLng(layerObj.layer.bbox[1], layerObj.layer.bbox[0]),
+        latLng(layerObj.layer.bbox[3], layerObj.layer.bbox[2]),
+      );
+      maps[layerObj.mapId]?.fitBounds(bounds);
+    }
+
+    if (layerObj.layer.coastline) {
+      coastlineLayer = tileLayer.wms(config.public.geoserverUrl, {
+        transparent: true,
+        format: "image/png",
+        version: "1.3.0",
+        layers: "natural_earth:ne_10m_coastline",
+      });
+      maps[layerObj.mapId]?.addLayer(coastlineLayer);
+    }
+
+    activeLayers.value[layerObj.mapId] = layer;
+
+    addLegend(layerObj.mapId, layer.legend);
+  }
+
+  const addCircumpolarPlaces = (mapId: string) => {
+    const communities = {
+      Helsinki: {
+        lat: 60.1695,
+        lon: 24.9355,
+      },
+      Iqaluit: {
+        lat: 63.75,
+        lon: -68.5167,
+      },
+      Juneau: {
+        lat: 58.3019,
+        lon: -134.42,
+      },
+      Москва: {
+        lat: 55.6256,
+        lon: 37.6064,
+      },
+      Nuuk: {
+        lat: 64.1814,
+        lon: -51.6942,
+      },
+      Oslo: {
+        lat: 59.9127,
+        lon: 10.7461,
+      },
+      Reykjavík: {
+        lat: 64.1333,
+        lon: -21.9,
+      },
+      Tórshavn: {
+        lat: 62.012,
+        lon: -6.768,
+      },
+      Whitehorse: {
+        lat: 60.727,
+        lon: -135.074,
+      },
+      Yellowknife: {
+        lat: 62.4536,
+        lon: -114.37,
+      },
+    };
+
+    let data: {
+      type: "FeatureCollection";
+      features: {
+        type: string;
+        properties: { name: string };
+        geometry: { type: string; coordinates: number[] };
+      }[];
+    } = {
+      type: "FeatureCollection",
+      features: [],
+    };
+
+    for (const [key, value] of Object.entries(communities)) {
+      data.features.push({
+        type: "Feature",
+        properties: {
+          name: key,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [value.lon, value.lat],
+        },
+      });
+    }
+
+    circumpolarPlaces = $L.geoJSON(data, {
+      pointToLayer: function (feature, latlng) {
+        return $L.circleMarker(latlng, {
+          radius: 3,
+          fillColor: "black",
+          color: "black",
+          weight: 1,
+          opacity: 0.5,
+          fillOpacity: 0.8,
+          interactive: false,
+        });
+      },
+      onEachFeature: function (feature, layer) {
+        const label = $L.divIcon({
+          className: "label",
+          html: `<div style="white-space: nowrap; margin-left: 15px; margin-top: -4px; font-size: 13px;">${feature.properties.name}</div>`,
+        });
+        $L.marker((layer as L.Marker).getLatLng(), { icon: label }).addTo(
+          maps[mapId],
+        );
+      },
+    });
+    circumpolarPlaces.addTo(maps[mapId]);
+  };
+
   return {
-    create,
-    destroy,
+    toggleLayer,
     activeLayers,
+    create,
+    setLegendItems,
+    destroy,
+    addLegend,
   };
 });
