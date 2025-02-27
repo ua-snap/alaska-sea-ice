fead8339225ad62fcae433010230c40a2ec81d42
Bob Torgerson <rltorgerson@alaska.edu>
2025-02-27 09:33:05 -0900

    Adds skip month / year buttons to LayerFields.

--- components/LayerFields.vue
+++ components/LayerFields.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="layer-fields">
     <div class="field">
+      <button @click="decreaseYear" class="arrow-button">≪</button>
+      <button @click="decreaseMonth" class="arrow-button"><</button>
+    </div>
+
+    <div class="field">
       <label for="month">Month:</label>
       <select v-model="month" id="month">
         <option
@@ -20,6 +25,11 @@
 
     <div class="field">
       <button @click="submit" class="button is-primary">Submit</button>
+    </div>
+
+    <div class="field">
+      <button @click="increaseMonth" class="arrow-button">></button>
+      <button @click="increaseYear" class="arrow-button">≫</button>
     </div>
   </div>
 </template>
@@ -48,21 +58,52 @@ const monthNames = [
   "Nov",
   "Dec",
 ];
-const month = ref<number | null>(null);
-const year = ref<number | null>(null);
+
+const month = ref<number>(0);
+const year = ref<number>(1950);
 
 const submit = () => {
   emit("submitLayerConfig", { month: month.value, year: year.value });
+};
+
+const decreaseYear = () => {
+  year.value -= 1;
+  submit();
+};
+
+const decreaseMonth = () => {
+  if (month.value === 0) {
+    month.value = 11;
+    year.value -= 1;
+  } else {
+    month.value -= 1;
+  }
+  submit();
+};
+
+const increaseMonth = () => {
+  if (month.value === 11) {
+    month.value = 0;
+    year.value += 1;
+  } else {
+    month.value += 1;
+  }
+  submit();
+};
+
+const increaseYear = () => {
+  year.value += 1;
+  submit();
 };
 </script>
 
 <style scoped>
 .layer-fields {
   display: flex;
-  justify-content: space-between;
-  gap: 20px;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
   width: 100%;
-  margin-left: -10px;
 }
 
 .field {
@@ -77,14 +118,32 @@ const submit = () => {
 }
 
 input {
-  width: 60px;
+  width: 70px;
   padding: 5px;
   height: 30px;
   box-sizing: border-box;
   margin: 0;
+  text-align: center;
 }
 
-button {
-  height: 30px;
+.arrow-button {
+  background-color: #444;
+  color: white;
+  border: none;
+  width: 35px;
+  height: 35px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: 0.3s;
+  border-radius: 5px;
+}
+
+.arrow-button:hover {
+  background-color: #666;
+}
+
+.button {
+  height: 35px;
+  padding: 5px 10px;
 }
 </style>
